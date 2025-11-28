6c4dd9956a52abb1670f7ea95bed78d3975d8a09
DennisB-AH <dennis.bleeker@ah.nl>
2025-11-28 18:43:28 +0100

    feat: basic footer & change main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
 import '../styles/globals.scss';
+import { Strong, Text } from '@radix-ui/themes';
+
+import css from './page.module.scss';
 
 export default function Home() {
+    /**
+     * TODO: Move text to constants file! (for translations etc.)
+     */
     return (
-        <div className="">
-            <h1>Welcome to UvO Amsterdam!</h1>
+        <div className={css.root}>
+            <Text align="center">
+                <Strong>UVO IN A FEW WORDS</Strong>
+                <br />
+                <br />
+                Welcome to UvO, the most fun student volleyball association in
+                Amsterdam!
+                <br />
+                <br />
+                UvO has been around for over 25 years! We play with eight
+                women's teams and five men's teams in the Nevobo competition and
+                also have two beginner training-only teams. The training
+                evenings are every Monday and Thursday at USC Universum (Science
+                Park). Home game evenings are mostly on Tuesdays in the
+                Wethouder Verheijhal and sometimes on Saturdays at USC
+                Universum. We close the training sessions and home play evenings
+                with a nice drink. UvO is a self-run association: together,
+                members make everything possible! 15 committees organize
+                tournaments, activities, trips and of course parties! With our
+                250 members, we have a great time all year round. Would you like
+                to know more about UvO? Send an email to
+                bestuur@uvo-amsterdam.nl.
+                <br />
+                <br />
+                Do you also want to play volleyball and party with us?
+                <br />
+                Sign-ups are now open for the next tryout sessions in January.
+                Sign-ups are now open for the next tryout sessions from August
+                25th to September 4th. Sign up at the bottom of the page and the
+                Technical Committee will contact you!
+            </Text>
         </div>
     );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,60 @@
-import type { FC } from 'react';
+import React, { type FC } from 'react';
+import { Link, Separator, Text } from '@radix-ui/themes';
 
 import css from './footer.module.scss';
 
 export const Footer: FC = () => {
+    /**
+     * TODO: Move info to constants file! (for translations etc.)
+     * Change Algemene voorwaarden link to actual page
+     */
+    const footerInfo = [
+        {
+            title: 'Over UvO Amsterdam',
+            contents: ['Een volleybal vereniging vol gezelligheid!'],
+        },
+        {
+            title: 'Adres',
+            contents: [
+                'Uvo Amsterdam',
+                'Science Park 306',
+                '1098 XH Amsterdam',
+            ],
+        },
+        {
+            title: 'Contact',
+            contents: [
+                'Bestuur: bestuur@uvo-amsterdam.nl',
+                'Technische Commissie (TC): tc@uvo-amsterdam.nl',
+            ],
+        },
+    ];
     return (
         <footer className={css.root}>
-            <h1>Footer</h1>
+            <div className={css.infoSection}>
+                {footerInfo.map(info => (
+                    <div key={info.title} className={css.bottomSection}>
+                        <Text>{info.title}</Text>
+                        {info.contents.map(content => (
+                            <Text key={content}>{content}</Text>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <Separator my="3" size="4" />
+            <div className={css.bottom}>
+                <div className={css.bottomSection}>
+                    <Text size="1">
+                        © COPYRIGHT {new Date().getFullYear()} UvO Amsterdam.
+                    </Text>
+                    <Text size="1">
+                        <Link href="/" highContrast>
+                            Algemene voorwaarden
+                        </Link>{' '}
+                        | Website made by itcommittee@uvo-amsterdam.nl
+                    </Text>
+                </div>
+            </div>
         </footer>
     );
 };
