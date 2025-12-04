d5b104e75926b9575a511b5eeaf127bbca2f3402
dennisbleeker <dennis.bleeker@ah.nl>
2025-12-04 22:26:08 +0100

    feat: add forms and socials data structures with links and icons

---
+++ src/constants/socials.ts
@@ -0,0 +1,17 @@
+export const SOCIALS = {
+    Instagram: {
+        title: 'Instagram',
+        link: 'https://www.instagram.com/uvoamsterdam/',
+        icon: 'IconBrandInstagram',
+    },
+    Facebook: {
+        title: 'Facebook',
+        link: 'https://www.facebook.com/UvO.Amsterdam',
+        icon: 'IconBrandFacebook',
+    },
+    LinkedIn: {
+        title: 'LinkedIn',
+        link: '/',
+        icon: 'IconBrandLinkedin',
+    },
+};
