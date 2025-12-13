b82c0ef060eb8e87749ffe129636ff9191f2e0da
dennisbleeker <dennis.bleeker@ah.nl>
2025-12-13 13:05:21 +0100

    feat: rename pages.ts to navigation.ts and update structure to use Pages type

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,52 +1,54 @@
-export const PAGES: Record<string, Page> = {
-    HOME: {
+import type { Pages } from '@interfaces/page';
+
+export const NAVIGATION: Pages = [
+    {
         link: '/',
         title: 'Welcome',
         subtitle: 'Welcome to the UvO Amsterdam website!',
     },
-    MERCH: {
+    {
         link: '/merch',
         title: 'Merch',
         subtitle: 'Buy our merchandise',
     },
-    TICKETS: {
+    {
         link: '/tickets',
         title: 'Tickets',
         subtitle: 'Get your tickets',
     },
-    SIGN_UP: {
+    {
         link: '/sign-up',
         title: 'Sign-up',
         subtitle: 'Join UvO Amsterdam',
     },
-    COMMITTEES: {
+    {
         link: '/committees',
         title: 'Committees',
         subtitle: 'Discover our committees',
     },
-    ARTICLES_OF_ASSOCIATION: {
+    {
         link: '/articles-of-association',
         title: 'Articles of Association',
         subtitle: 'Read our articles of association',
     },
-    MEMBER_INFO: {
+    {
         link: '/member-info',
         title: 'Member Info',
         subtitle: 'Information for members',
     },
-    TEAMS: {
+    {
         link: '/teams',
         title: 'Teams',
         subtitle: 'Meet our teams',
     },
-    TRAINING: {
+    {
         link: '/training',
         title: 'Training',
         subtitle: 'Training schedule and info',
     },
-    CONTACT: {
+    {
         link: '/contact',
         title: 'Contact',
         subtitle: 'Get in touch with us',
     },
-};
+];
--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,5 +1,7 @@
-interface Page {
+export interface Page {
     title: string;
     link: string;
     subtitle: string;
 }
+
+export type Pages = Page[];
