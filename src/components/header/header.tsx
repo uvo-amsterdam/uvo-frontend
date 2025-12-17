768bbf4c4422d5b2e1f709464fdd516a5e79541c
dennisbleeker <dennis.bleeker@ah.nl>
2025-12-17 14:36:26 +0100

    feat: setup routing & pages

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,6 @@
 import type { FC } from 'react';
+import { NAVIGATION } from '@constants/navigation';
+import { Link } from '@radix-ui/themes';
 import Image from 'next/image';
 
 import css from './header.module.scss';
@@ -11,11 +13,25 @@ export const Header: FC = () => {
                 content="minimum-scale=1.0, initial-scale=1.0, width=device-width, user-scalable=no"
             />
             <div className={css.imageContainer}>
-                <Image
-                    src="/images/logo/uvo-logo.jpeg"
-                    alt="UvO logo"
-                    fill={true}
-                />
+                <Link href={'/'}>
+                    <Image
+                        src="/images/logo/uvo-logo.jpeg"
+                        alt="UvO logo"
+                        fill={true}
+                    />
+                </Link>
+            </div>
+            <div className={css.navContainer}>
+                {NAVIGATION.map(navLink => (
+                    <Link
+                        key={navLink.link}
+                        href={navLink.link}
+                        underline="hover"
+                        className={css.navLink}
+                    >
+                        {navLink.title}
+                    </Link>
+                ))}
             </div>
         </header>
     );
