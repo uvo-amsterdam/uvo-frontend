0bd888364b23ee9c92604e89deed1e760259c58d
DanilaRomanov <thedanilaromanov@gmail.com>
2026-04-08 16:49:01 +0200

    Fix: New solution only applies on homepage to reduce the number of site data destructs.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     async headers() {
         return [
             {
-                source: '/:path*',
+                source: '/',
                 has: [
                     {
                         type: 'header',
