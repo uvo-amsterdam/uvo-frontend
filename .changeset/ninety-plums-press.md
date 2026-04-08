91b9a70fc015a7e1bf88899bec886624d5e2d86e
DanilaRomanov <thedanilaromanov@gmail.com>
2026-04-08 12:51:54 +0200

    Fix: Added cache headers to force browsers to remake website

---
+++ .changeset/ninety-plums-press.md
@@ -0,0 +1,5 @@
+---
+"uvo-frontend": patch
+---
+
+Added cache headers to force browsers to refetch website
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'accept',
+                        value: '.*text/html.*',
+                    },
+                ],
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-cache, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+            {
+                source: '/_next/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
