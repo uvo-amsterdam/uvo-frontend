166df835c0fc871afbc13fa981819d2b91cf0895
DanilaRomanov <thedanilaromanov@gmail.com>
2026-04-10 18:04:11 +0200

    fix: Bug with infinite api calls

--- src/hooks/use-training-schedules.ts
+++ src/hooks/use-training-schedules.ts
@@ -1,11 +1,13 @@
 import type { TrainingSchedules } from '@interfaces/training-schedule';
 import { useApiFetch } from './use-api-fetch';
 
+const identityParser = (raw: TrainingSchedules): TrainingSchedules => raw;
+
 export const useTrainingSchedules = () => {
     const { data, loading, error } = useApiFetch<
         TrainingSchedules,
         TrainingSchedules | null
-    >('/api/training-schedules', raw => raw, null);
+    >('/api/training-schedules', identityParser, null);
 
     return { data, loading, error };
 };
